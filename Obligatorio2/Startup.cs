d5e5c970849109ce0c5df03b9fcf4297a1bd7c34
gpicart <gpicart@despegar.com>
2018-06-18 19:42:25 -0300

    ìnit

---
+++ Obligatorio2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Obligatorio2.Startup))]
+namespace Obligatorio2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
