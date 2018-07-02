48b19b920ff76b0ef994d0e8e2a7117041911b15
gpicart <gpicart@despegar.com>
2018-07-02 04:51:30 -0300

    Stage selector

--- Documentación/insert.sql
+++ Documentación/insert.sql
@@ -8,3 +8,6 @@ INSERT INTO CASES VALUES('email@email.com', '2011-04-12T00:00:00.000', 0, 1, 1)
 INSERT INTO CASES VALUES('rara@email.com', '2014-01-12T00:00:00.000', 1, 2, 2)
 INSERT INTO CASES VALUES('cccc@email.com', '2016-09-03T00:00:00.000', 1, 3, 3)
 INSERT INTO CASES VALUES('vvvv@email.com', '2018-02-03T00:00:00.000', 0, 4, 4)
+
+UPDATE Stages SET documentName = 'pepe.jpeg' WHERE id = 1;
+UPDATE Stages SET documentName = 'lala.jpeg' WHERE id = 2;
---
+++ Obligatorio2/Controllers/StageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Obligatorio2.Models;
+using Obligatorio2.ViewModels;
+
+namespace Obligatorio2.Controllers
+{
+    public class StageController : Controller
+    {
+        ManagementContext db = new ManagementContext();
+        // GET: Stage
+        public ActionResult Index(string id)
+        {
+
+            if (Session["idUser"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            List<Stage> stages = db.AppProcedure.Find(id).Stages.ToList();
+            List<StageViewModel> response = new List<StageViewModel>();
+
+            foreach(var s in stages)
+            {
+                StageViewModel viewM = new StageViewModel();
+                viewM.Path = "file:///" + Server.MapPath((@"~\App_Data\docs\") + s.documentName);
+                viewM.Stage = s;
+
+                response.Add(viewM);
+            }
+
+            return View(response);
+        }
+    }
+}
